b632d72c23f209e5c9b59a2529543e1075735b55
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-06-17 16:50:15 +0200

    exercise 4 and 5 done

--- expert.php
+++ expert.php
@@ -30,10 +30,23 @@ new_exercise(4);
 // The print_r($week) should give:  Array ( [0] => mon [1] => tues [2] => wednes [3] => thurs [4] => fri [5] => satur [6] => sun )
 // Look up whats going wrong with this code, and then fix it, with ONE CHARACTER!
 
-foreach($week as $day) {
+foreach($week as &$day) {
     $day = substr($day, 0, strlen($day)-3);
 }
-
 print_r($week);
+
+new_exercise(5);
+// === Exercise 5 ===
+// The array should be printing every letter of the alfabet (a-z) but instead it does that + aa-yz
+// Fix the code so the for loop only pushes a-z in the array
+
+$arr = [];
+foreach(range('a','z')as $letter){
+    array_push($arr, $letter);
+}
+
+print_r($arr); // Array ([0] => a, [1] => b, [2] => c, ...) a-z alfabetical array
+
+
 
 
